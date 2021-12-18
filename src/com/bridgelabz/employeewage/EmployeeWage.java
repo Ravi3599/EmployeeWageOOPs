bf73c634e318b9af99ae3d638e09c7e55fdb9188
Ravi3599 <ravikantdukre2018@gmail.com>
2021-12-18 22:08:23 +0530

    UPDATED: employee present or absent

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -3,9 +3,24 @@ package com.bridgelabz.employeewage;
 public class EmployeeWage {
 
 	public static void main(String[] args) {
-
-		System.out.println("Welcome to Employee Wage Computation Program");
-
-	}
-
+			Attendence attendence = new Attendence();
+			attendence.empCheck();
+		}
+}
+class Attendence {
+		public static final int IS_PRESENT = 1;
+		public static final int IS_ABSENT = 0;
+		public void empCheck() {
+			int empCheck = (int)Math.floor(Math.random() * 10) % 2;
+			switch(empCheck) {
+			case IS_PRESENT: 
+				System.out.println("Employee is present");
+				break;
+			case IS_ABSENT:
+				System.out.println("Employee is absent");
+				break;
+			default:
+				System.out.println("Welcome");
+			}
+		}
 }
